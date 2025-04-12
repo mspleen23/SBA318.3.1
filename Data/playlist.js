c0b09d98b8ebb8f0a541223a71d4dbc4b3e07c06
Melissa <mespleen@gmail.com>
2025-04-11 20:02:13 -0400

    Post Updates

--- Data/playlist.js
+++ Data/playlist.js
@@ -3,28 +3,28 @@ const songs = require("./songs");
 const playlist = [
 { 
     id: 1, 
-    name: "The It Girls of the 90s", 
+    playlistTitle: "The It Girls of the 90s", 
     genre: '90s Rnb',
     songId: [4,5,6]
 },
 
 {
     id:2,
-    name: 'RnB Girlies',
+    playlistTitle: 'RnB Girlies',
     genre: 'Rnb Love Songs Mix',
     songId: [1,2,3]
 },
 
 {
     id:3,
-    name: "Bob Marley Favs",
+    playlistTitle: "Bob Marley Favs",
     genre: "Roots Reggae",
     songId:[7,8,9]
 },
 
 {
     id:4,
-    name: "Song of the Day",
+    playlistTitle: "Song of the Day",
     genre: "Various", 
     songId:[10]    
 },
--- Routes/playlistRoute.js
+++ Routes/playlistRoute.js
@@ -3,9 +3,27 @@ const router= express.Router();
 
 const playlist= require("../Data/playlist");
 
-router.get("/", (req, res)=>{
+router.route('/')
+.get((req, res)=>{
     res.json(playlist)
 })
+
+.post((req,res)=>{
+    if (req.body.playlistTitle && req.body.genre && req.body.songId){
+        if(playlist.find ((p)=>p.playlistTitle == req.body.playlistTitle)){
+            return res.json({error:"Playlist Already Exist"})
+        }
+    
+    const playlist ={
+     id: playlist[playlist.length - 1].id + 1,
+     ...req.body
+    }
+
+    playlist.push(playlist);
+    res.join(playlist);
+    } else res.json({error: "Not enough data, sorry try again"})
+});
+
 
 
 module.exports= router; 
